67b9396a0681945b31785d0265da18e441a40f0d
Edy Nascimento <nascoliedy@gmail.com>
2026-05-27 13:24:02 -0400

    Iniciando o projeto

---
+++ src/main/java/com/stock_control/api/StockControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.stock_control.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock_control/api/StockControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_control.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
